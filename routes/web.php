0d01ab7603b5507374764c4f34480d4d9f964a86
NguyenThiHongTham99 <nguyenthihongtham170199@gmail.com>
2019-07-31 08:40:33 +0700

    tham

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('pages.trangchu');
 });
+Route::prefix('/')->group(function () {
 
-Route::get('trang-chu', [
-	'as' =>'trang-chu',
-	'uses' => 'Controller_1@get_trangchu'
-]);
+    Route::get('blog',function(){
+        return view('pages.blog');
+    });
+    Route::get('bai_hoc_cuoc_song',function(){
+        return view('pages.bai_hoc_cuoc_song');
+    });
+    Route::get('chia_se_cuoi_tuan',function(){
+        return view('pages.chia_se_cuoi_tuan');
+    });
+    Route::get('guong_thanh_cong',function(){
+        return view('pages.guong_thanh_cong');
+    });
+    Route::get('kien_thuc_kinh_doanh',function(){
+        return view('pages.kien_thuc_kinh_doanh');
+    });
+    Route::get('phat_trien_ca_nhan',function(){
+        return view('pages.phat_trien_ca_nhan');
+    });
+    Route::get('phong_cach_song',function(){
+        return view('pages.phong_cach_song');
+    });
+  
+});
+   
 
-Route::get('tintuc', [
-	'as' =>'tintuc',
-	'uses' => 'Controller_1@get_tintuc'
-]);
-
-Route::get('chitiettintuc', [
-	'as' =>'chitiettintuc',
-	'uses' => 'Controller_1@get_chitiettintuc'
-]);
-
-Route::get('gioithieu', [
-	'as' =>'gioithieu',
-	'uses' => 'Controller_1@get_gioithieu'
-]);
